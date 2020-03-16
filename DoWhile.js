be23b11e5f01dd4609d4364c095e3cd90dac05ce
DanielJacob <danieljacob196015@gmail.com>
2020-03-16 13:36:35 +0100

    .

--- DoWhile.js
+++ DoWhile.js
@@ -19,6 +19,7 @@ function InsertArray(array, index, number){
     return array
 }
 
+// This funtion uses Linear time complexity, steps = size of array.
 
 module.exports.InsertArray = InsertArray;
 
