b739e7b800f2b874a20805518b3da557943ec64c
DanielJacob <danieljacob196015@gmail.com>
2020-03-17 14:20:20 +0100

    added Insert Double Class

--- InsertDouble.js
+++ InsertDouble.js
@@ -2,15 +2,23 @@ function InsertDouble(num1, num2, arrayList){
 
     //custom array 
     var array = [num1, num2];
+    var z = arrayList.length;
     
+    for(var x = 0, y = array.length; x < z ; x++, y++)
+    { 
+        
+        console.log("value of x: " + x)
+        console.log("value of y: " + y)
+        array[y] = arrayList[x];
+       
 
-    for(var x = 0, y = array.length; x < arrayList.length; x++, y++)
-    {
-        array[y+x] = arrayList[x];
+        console.log("Array After Insertion " + array)
+        
     }
 
-    return array;
-
+    return array
 }
+
+//This function's time complexity is linear O(n) where n the size of the array that is passed to the method.
 
 module.exports.InsertDouble = InsertDouble;
